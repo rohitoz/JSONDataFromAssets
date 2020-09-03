4b48e3801cdd76b585e239b89e7265b15ea7761b
Rohitoz <rohit.geeky@gmail.com>
2020-09-03 17:52:39 +0530

    first commit

---
+++ Round3_JsonParse_RohitNikam/JsonDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JsonDataTableViewCell.swift
+//  Round3_JsonParse_RohitNikam
+//
+//  Created by Geekyworks on 03/09/20.
+//  Copyright © 2020 Rohit Nikam. All rights reserved.
+//
+
+import UIKit
+
+class JsonDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
